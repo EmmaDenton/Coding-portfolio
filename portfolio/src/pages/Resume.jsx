90a1032e334b1634fba76b7a06418f3185884f77
EmmaDenton <edenton34@gmail.com>
2024-01-16 21:10:53 +1100

    added proficiencies to resume page

--- portfolio/src/pages/Resume.jsx
+++ portfolio/src/pages/Resume.jsx
@@ -4,10 +4,21 @@ function Resume() {
   return (
     <div id='resumecontainer'>
         <div id="resume-title">My Resume</div>
-        <a className="resumedownload" href="./src/assets/ExampleResume.pdf" download="ExampleResume.pdf">
+      <ul className='skillslist'>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Javascript</li>
+      <li>Web APIs</li>
+      <li>Node.js</li>
+      <li>Express.js</li>
+      <li>MySQL</li>
+      <li>NoSQL</li>
+      <li>MondoDB</li>
+      <li>React</li>
+      </ul>
+      <a className="resumedownload" href="./src/assets/ExampleResume.pdf" download="ExampleResume.pdf">
         Download my resume
       </a>
-      <li className='skillslist'></li>
     </div>
   );
 }
