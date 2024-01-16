f1d5233076f98f24cf41dc71b1222fbfb01482cc
EmmaDenton <edenton34@gmail.com>
2024-01-16 19:18:29 +1100

    added html for resume page

--- portfolio/src/pages/Resume.jsx
+++ portfolio/src/pages/Resume.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 function Resume() {
   return (
-    <div>
-        <div>My Resume</div>
+    <div id='resumecontainer'>
+        <div id="resume-title">My Resume</div>
         <a className="resumedownload" href="../assets/ExampleResume.pdf" download="ExampleResume.pdf">
         Download my resume
       </a>
-      <div>My Skills</div>
-      <p></p>
+      <li className='skillslist'></li>
     </div>
   );
 }
