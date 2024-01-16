e2f331b11149e6260706eda4f1989aeae2bb64f0
EmmaDenton <edenton34@gmail.com>
2024-01-16 19:58:53 +1100

    added links to footer

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -3,10 +3,11 @@ import { useState } from 'react';
 function Footer({ currentPage, handlePageChange }) {
 return (
     <ul id="contactme-nav">
-    <li className="contact-link" href="https://github.com/EmmaDenton/"><a>Github</a></li> 
-    <li className="contact-link"><a>Codepen</a></li> 
-    <li className="contact-link"><a>Email</a></li>
-    <li className="contact-link"><a>Linkedin</a></li>
+    <li className="contact-link"><a href="https://github.com/EmmaDenton/" target="_blank" rel="noopener noreferrer">Github</a></li> 
+    <li className="contact-link"><a href="https://codepen.io/EmmaDenton/" target="_blank" rel="noopener noreferrer">Codepen</a></li> 
+    <li className="contact-link"><a href="mailto: edenton34@gmail.com" target="_blank" rel="noopener noreferrer">Email</a></li>
+    <li className="contact-link"><a href="https://www.linkedin.com/in/emma-denton-1283b0182/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
+
     </ul>
   );
 }
